7210be5cd716ecdab90927a2dfa2cbab6f40a239
hmpark94 <hmpark94@users.noreply.github.com>
2022-05-23 14:16:42 +0900

    Create example.py

---
+++ example/example.py
@@ -0,0 +1,14 @@
+import streamlit
+import FRAKE
+
+st.title("Keyword Extraction Demo: FRAKE")
+st.write("This is a keyword extraction demo using FRAKE combined with Graph Features.")
+
+
+kw = FRAKE.KeywordExtractor(lang='en',hu_hiper=0.4,Number_of_keywords=10)
+
+text = st.text_input("입력")
+
+if st.button('출력(키워드 리스트)'):
+  kws = kw.extract_keywords(text)
+  st.write(kws)
