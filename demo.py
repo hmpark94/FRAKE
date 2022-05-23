2e5b7839d33da6db0ee1d5a80d3f41d320a28e70
hmpark94 <hmpark94@users.noreply.github.com>
2022-05-23 14:51:40 +0900

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import networkx as nx
 import FRAKE
+import nltk
+
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
 
 st.title("Keyword Extraction Demo: FRAKE")
 st.write("This is a keyword extraction demo using FRAKE combined with Graph Features.")
