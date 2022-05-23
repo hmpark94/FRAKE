16674dd2a2c1285b52e32392347b389c011e8a0e
hmpark94 <hmpark94@users.noreply.github.com>
2022-05-23 14:55:32 +0900

    Update demo.py

--- demo.py
+++ demo.py
@@ -7,13 +7,13 @@ nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 
 st.title("Keyword Extraction Demo: FRAKE")
-st.write("This is a keyword extraction demo using FRAKE combined with Graph Features.")
+st.write("그래프 구조 정보를 활용하는 키워드 추출 알고리즘의 데모입니다.")
 
 
 kw = FRAKE.KeywordExtractor(lang='en',hu_hiper=0.4,Number_of_keywords=10)
 
-text = st.text_input("입력")
+text = st.text_area("입력")
 
-if st.button('출력(키워드 리스트)'):
+if st.button('키워드 추출'):
   kws = kw.extract_keywords(text)
   st.write(kws)
